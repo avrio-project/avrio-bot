3c37d2e420e90fc7ccb8ed21da507a081ef5e171
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-03-07 13:31:04 +0000

    Update create_dbs.js

--- create_dbs.js
+++ create_dbs.js
@@ -4,21 +4,15 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("tipbot-balances");
-//  dbo.createCollection("users", function(err, res) {
-  //  if (err) throw err;
-    //console.log("Users table created!");
-    //db.close();
-  //});
-    var myobj = { name: "null", address: "null", balance: 0, locked: 0 };
+  dbo.createCollection("users", function(err, res) {
+    if (err) throw err;
+    console.log("Users table created!");
+    db.close();
+  });
+  var myobj = { name: "null", address: "null", balance: 0, locked: 0 };
   dbo.collection("users").insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 entry inserted");
-    db.close();
-  });
-     var query = { address: "null" };
-  dbo.collection("users").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
     db.close();
   });
 }); 
